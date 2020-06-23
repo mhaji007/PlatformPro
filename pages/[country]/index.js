ecdcb8073a7aa8109b849ebf29602309c09dadb6
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-06-22 22:07:08 -0400

    Fetch data on client

--- pages/[country]/index.js
+++ pages/[country]/index.js
@@ -1,3 +1,16 @@
-const CountryTest = () => <h1> This is a country test </h1>
+import {useEffect} from 'react';
+import axios from 'axios';
+
+const CountryTest = () => {
+    useEffect(() => {
+        axios.get('http://api.tvmaze.com/schedule?country=US&date=2014-12-01')
+            .then(response => console.log(response.data))
+
+    }, [])
+
+    return (
+    <h1>This is a country test</h1>
+    )
+}
 
 export default CountryTest;
