227d1ac3550993c27146225ebb8e3e6ea279eb70
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-06-23 13:29:37 -0400

    Fetch data serverside

--- pages/[country]/index.js
+++ pages/[country]/index.js
@@ -1,16 +1,17 @@
 import {useEffect} from 'react';
 import axios from 'axios';
 
-const CountryTest = () => {
-    useEffect(() => {
-        axios.get('http://api.tvmaze.com/schedule?country=US&date=2014-12-01')
-            .then(response => console.log(response.data))
+const CountryTest = () => { 
+useEffect(() => {
+    axios.get('http://api.tvmaze.com/schedule?country=US&date=2014-12-01')
+    .then(response => console.log(response.data))
 
-    }, [])
+}, [])
 
-    return (
-    <h1>This is a country test</h1>
-    )
+
+return (
+<h1> This is a country test </h1>
+)
 }
 
 export default CountryTest;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,30 @@
+import {useEffect} from 'react';
+import axios from 'axios';
 
-const Home = () => {
-  return (
-    <div>
-      This is my Home Page
-    </div>
+const Home = (props) => {
+    
+    // console.log("TCL: Home -> props", props)
+    
+    // useEffect(() => {
+
+    //     axios.get('http://api.tvmaze.com/schedule?country=US&date=2014-12-01')
+    //         .then(response => console.log(response.data))
+
+    // }, [])
+
+    return (
+    <h1>This is a country test</h1>
     )
-
 }
 
-export default Home
+Home.getInitialProps = async () => {
+    const response = await axios.get('http://api.tvmaze.com/schedule?country=US&date=2014-12-01')
+    console.log("TCL:Home.getInitialProps -> response", response.data)
 
+    return {
+        test: 'testing'
+    }
+}
+
+
+export default Home;
