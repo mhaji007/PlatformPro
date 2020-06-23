a2e0845886fed9fd58fba11dbe2b9a0df00e69a2
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-06-23 15:52:47 -0400

    Refactor code. Fetch data for other countries

--- pages/[country]/index.js
+++ pages/[country]/index.js
@@ -1,17 +1,39 @@
-import {useEffect} from 'react';
 import axios from 'axios';
 
-const CountryTest = () => { 
-useEffect(() => {
-    axios.get('http://api.tvmaze.com/schedule?country=US&date=2014-12-01')
-    .then(response => console.log(response.data))
-
-}, [])
-
-
-return (
-<h1> This is a country test </h1>
-)
+const Home = ({shows}) => {
+    
+  //  console.log("TCL: Home -> props", shows)
+  
+  // useEffect(() => {
+    
+    //     axios.get('http://api.tvmaze.com/schedule?country=US&date=2014-12-01')
+    //         .then(response => console.log(response.data))
+    
+    // }, [])
+    
+    const renderShows = () => {
+      return shows.map((show, index) => {
+        return (
+          <li key={index}>{show.show.name}</li>
+        )
+      })
+    }
+    return (
+    <ul className="tvshows">{renderShows()}</ul>
+    )
 }
 
-export default CountryTest;
+
+Home.getInitialProps = async ({query}) => {
+
+    const {country} = query || 'us';
+    
+    const response = await axios.get(`http://api.tvmaze.com/schedule?country=${country}&date=2014-12-01`)
+    
+    return {
+        shows: response.data
+    }
+}
+
+
+export default Home;
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,12 @@
-import axios from 'axios';
 
-const Home = ({shows}) => {
-    
-     console.log("TCL: Home -> props", shows)
-    
-    // useEffect(() => {
-
-    //     axios.get('http://api.tvmaze.com/schedule?country=US&date=2014-12-01')
-    //         .then(response => console.log(response.data))
-
-    // }, [])
-
-    return (
-    <h1>This is a country test</h1>
+ Home = () => {
+  return (
+    <div>
+      This is my Home Page
+    </div>
     )
+
 }
 
-Home.getInitialProps = async () => {
-    const response = await axios.get('http://api.tvmaze.com/schedule?country=US&date=2014-12-01')
-    return {
-        shows: response.data
-    }
-}
+export default Home
 
-
-export default Home;
