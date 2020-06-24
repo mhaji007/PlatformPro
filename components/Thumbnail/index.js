a1b5c8452f29ed373457ac5b5ac6dbfc3bf7f32e
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-06-24 18:56:29 -0400

    Provide default thumbnail image for shows with no image

--- components/Thumbnail/index.js
+++ components/Thumbnail/index.js
@@ -1,6 +1,6 @@
 import ThumbnailStyles from './styles';
 
-const Thumbnail = ({imageUrl, caption}) => {
+const Thumbnail = ({imageUrl = 'http://via.placeholder.com/210x295?text=?', caption}) => {
     return (
         <div className="thumbnail"> 
             <img src={imageUrl} className="thumbnail__image"/>
