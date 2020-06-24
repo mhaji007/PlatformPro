2ca4ad2e99db33d99e6b2637d5adc3ae6e6bb7bd
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-06-24 17:38:02 -0400

    Extract styles to a separate file

--- components/Thumbnail/index.js
+++ components/Thumbnail/index.js
@@ -1,17 +1,14 @@
+import ThumbnailStyles from './styles';
+
 const Thumbnail = ({imageUrl, caption}) => {
     return (
         <div className="thumbnail"> 
             <img src={imageUrl} className="thumbnail__image"/>
             <h3 className="thumbnail__caption">{caption}</h3>
 
-        <style jsx>{`
-            .thumbnail__image {
-                width: 100%;
-            }
-            .thumbnail__caption {
-                text-align:center;
-            }
-        `}</style>
+        <style jsx>
+            {ThumbnailStyles}
+       </style>
         </div>
 
     );
