8012d20f9af629b4ad396b4cd8f9b240ce3b37f8
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-06-24 16:11:22 -0400

    Add simple style to thumbnails

--- components/Thumbnail/index.js
+++ components/Thumbnail/index.js
@@ -3,9 +3,18 @@ const Thumbnail = ({imageUrl, caption}) => {
         <div className="thumbnail"> 
             <img src={imageUrl} className="thumbnail__image"/>
             <h3 className="thumbnail__caption">{caption}</h3>
+
+        <style jsx>{`
+            .thumbnail__image {
+                width: 100%;
+            }
+            .thumbnail__caption {
+                text-align:center;
+            }
+        `}</style>
         </div>
 
-    )
-}
+    );
+};
 
 export default Thumbnail;
