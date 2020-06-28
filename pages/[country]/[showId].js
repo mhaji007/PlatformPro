628eaa61703a22a6a72551bc32e58c5c5a0dc7e4
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-06-27 21:32:37 -0400

    Customize default error handling

--- pages/[country]/[showId].js
+++ pages/[country]/[showId].js
@@ -1,12 +1,19 @@
 import axios from 'axios';
 import parse from 'html-react-parser';
 import Cast from '../../components/Cast';
+import Error from 'next/error';
 
 
-const ShowDetails = ({show}) => {
+const ShowDetails = ({show = {}, statusCode}) => {
 
     const {name, image, summary, _embedded} = show;
     
+    if (statusCode) {
+       
+        return <Error statusCode={statusCode} title="There was a problem"/>
+    }
+
+
     return (
         <div className="show-details">
             <div className="show-details__poster"
@@ -30,12 +37,19 @@ const ShowDetails = ({show}) => {
 };
 
 ShowDetails.getInitialProps = async({query}) => {
+
+    try {
     const {showId} = query;
     const response = await axios.get(`http://api.tvmaze.com/shows/${showId}?embed=cast`)
 
     return {
         show: response.data
     }
+} catch (error) {
+    return {
+        statusCode: error.response?error.response.status : 500
+    }
+}
 }
 
 export default ShowDetails;
