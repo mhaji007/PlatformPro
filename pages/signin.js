0ec8ba026779a5d5a5c3fa56eefc696fe1c329c5
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-07-11 23:41:24 -0400

    Create sign in form

--- pages/signin.js
+++ pages/signin.js
@@ -1,7 +1,42 @@
+import {useState} from 'react';
+
+
+const initialState = {
+    email: '',
+    password: ''
+};
+
+
+
 const Signin = () => {
+
+    const [signinInfo, setSigninInfo] =  useState(initialState);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log('Submitting!!')
+    }
+
+    const handleInputChange = (e) => {
+        console.log(e.target.value);
+    }
+
+
     return  (
-        <div className="signin"> Sign in </div>
+        <div className="signin"> 
+        
+        <form onSubmit = {handleSubmit}>
+            <input name = "eamil" placeholder="Enter your email" value = {signinInfo.email} onChange={handleInputChange}></input>
+            <input name = "password" placeholder="Enter your password" value = {signinInfo.password} onChange={handleInputChange}></input>
+
+            <submit type="submit"> Submit </submit>
+        </form>
+        
+        
+         </div>
+
     )
-}
+};
 
 export default Signin
