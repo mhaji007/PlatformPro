17ab66f6be4e68430ae97f1c496a74f8022e8945
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-07-11 23:17:38 -0400

    Create authenticate method in withAuthorization

--- utils/withAuthorization.js
+++ utils/withAuthorization.js
@@ -1,12 +1,30 @@
 import {Component} from 'react';
+import cookies from 'nookies';
+import Router from 'next/router';
+
+const authenticate = context => {
+    const {token} = cookies.get(context);
+    // Check if cookie is present 
+    // on serverside
+    if (context.req && !token) {
+        context.res.writeHead(302, {location:'/signin'});
+        context.end();
+    }
+
+    if (!token) {
+        Router.push('/signin')
+    }
+    return token;
+}
 
 const withAuthorization = (WrappedComponent) => {
     return class extends Component {
         static async getInitialProps(context) {
+            const token = authenticate(context);
             const componentProps = WrappedComponent.getInitialProps &&
                 (await WrappedComponent.getInitialProps(context));
 
-            return {...componentProps};
+            return {...componentProps, token};
         }
         render () {
             return <WrappedComponent {...this.props}/>
