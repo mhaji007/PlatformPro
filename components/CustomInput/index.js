0b0bcbcb6efc295d995272f0234ef7553002920d
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-07-15 21:01:19 -0400

    Handle errors on blur

--- components/CustomInput/index.js
+++ components/CustomInput/index.js
@@ -1,14 +1,33 @@
-const CustomInput = ({type, name, placeholder='', value, onChange=()=>{}}) => {
-    return (
-<div className="custom_input">
-    <input
-        type={type}
-        name={name}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}></input>
-</div>
-    )
-}
+import { useState } from 'react';
+
+const CustomInput = ({
+	name,
+	placeholder = '',
+	value,
+	onChange,
+	type = 'text',
+	onBlur = () => {}
+}) => {
+	const [error, setError] = useState('');
+
+	const handleBlur = () => {
+		const isValid = onBlur && onBlur(value);
+		isValid ? setError('') : setError(`Invalid ${name}`);
+	};
+
+	return (
+		<div>
+			<input
+				name={name}
+				placeholder={placeholder}
+				value={value}
+				onChange={onChange}
+				type={type}
+				onBlur={handleBlur}
+			/>
+			{error && <div className="error">{error}</div>}
+		</div>
+	);
+};
 
 export default CustomInput;
