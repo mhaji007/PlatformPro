7eab035ebd7f22baa6aef49d770560259d2c73f8
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-07-16 22:41:42 -0400

    Intercept defective url

--- server.js
+++ server.js
@@ -4,10 +4,18 @@ const next = require('next');
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({dev});
-const handle = app.getRequestHandler();
+const handler = app.getRequestHandler();
 
 app.prepare().then(() => {
     const server = express();
+
+    server.get(`/cast/:personId`, (req, res) => {
+        const {personId} = req.params;
+        console.log('TCL: req.params', req.params);
+        return app.render(req, res, '/cast', {personId})
+    })
+
+
 
     server.all('*', (req, res) => {
         return handler(req, res)
